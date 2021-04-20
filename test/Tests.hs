cb32b9c7dbc5321a9eb69cf0053002225814bca9
Prem Muthedath <prem.muthedath@gmail.com>
2021-04-21 00:30:35 +0530

    test/Tests.hs: 1. + `Max` test case; 2. `forAll (listOf1 arbitrary)` generates non-empty list for min & max test cases in `qcProperty`; 3. `classify_` -> `classifys`; 4. `Library` -> `Model`.

--- test/Tests.hs
+++ test/Tests.hs
@@ -8,13 +8,14 @@ import Quicksort (qsortFunctions)
 type QCSortFunction = ([Int] -> [Int])
 type QCProperty = [Int] -> Property
 
-data TestCase = Ordering | Invariance | Library | Min deriving (Eq, Enum)
+data TestCase = Ordering | Invariance | Model | Min | Max deriving (Eq, Enum)
 
 instance Show TestCase where
   show Ordering    = "*** property: ordered ***"
   show Invariance  = "*** property: invariance ***"
-  show Library     = "*** property: library sort ***"
+  show Model       = "*** property: model sort equivalence ***"
   show Min         = "*** property: minimum ***"
+  show Max         = "*** property: maximum ***"
 
 testCases :: [TestCase]
 testCases = [toEnum 0 :: TestCase ..]
@@ -24,14 +25,18 @@ qcProperties f = map (\tc -> (tc, qcProperty tc)) testCases
   where qcProperty :: TestCase -> QCProperty
         qcProperty testCase xs = let result = f xs in
             case testCase of
-                 Ordering   -> classify_ xs $ ordered result
-                 Invariance -> classify_ xs $ result == f result
-                 Library    -> classify_ xs $ result == sort xs
-                 Min        -> not (null xs) ==> classify_ xs $ head result == minimum xs
+                 Ordering   -> classifys xs $ ordered result
+                 Invariance -> classifys xs $ result == f result
+                 Model      -> classifys xs $ result == sort xs
+                 Min        -> forAll (listOf1 arbitrary) $
+                               \ys -> classifys ys $ head (f ys) == minimum ys
+                 Max        -> forAll (listOf1 arbitrary) $
+                               \ys -> classifys ys $ last (f ys) == maximum ys
 
-classify_ :: Testable prop => [Int] -> prop -> Property
-classify_ xs = classify (xs==[]) "empty" .
+classifys :: Testable prop => [Int] -> prop -> Property
+classifys xs = classify (xs==[]) "empty" .
                classify (length xs > 10) "has > 10 elements" .
+               classify (ordered xs) "pre-ordered" .
                classify (hasDups xs) "has duplicates"
 
 hasDups :: (Ord a) => [a] -> Bool
@@ -49,7 +54,7 @@ runQC f = mapM_(\(testCase, prop) ->
               ) $ qcProperties f
 
 defaultMain :: IO ()
-defaultMain = mapM_ (\(a,f) ->
+defaultMain = mapM_ (\(a, f) ->
     do putStrLn $ "\n--- " ++ show a ++ " ---"
        runQC f
     ) qsortFunctions
